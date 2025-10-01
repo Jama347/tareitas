1807e26c8eb371f7f517e896dbe471813858aee2
Jama347 <holajoaquin15@gmail.com>
2025-09-30 23:03:04 -0300

    Delete tarea2 directory

--- tarea2/actualizado/hud.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HUD_HPP
-#define HUD_HPP
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-
-void drawHUD(SDL_Renderer* renderer, TTF_Font* font, int hp, int mana, int energy, int enemyHp);
-
-#endif
--- tarea2/actualizado/readme 1.md
+++
@@ -1,5 +0,0 @@
-Poner en la consola de linux:
-
-sudo apt update
-sudo apt install libsdl2-dev libsdl2-ttf-dev
-
